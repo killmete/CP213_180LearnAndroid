2ed918e6b1a5a8d881199c8774f8b36ee5e516ed
killmete <205147509+killmete@users.noreply.github.com>
2026-01-27 16:22:21 +0700

    add HP

--- lab/app/src/main/java/com/example/a180learnandroid/MainActivity2.kt
+++ lab/app/src/main/java/com/example/a180learnandroid/MainActivity2.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a180learnandroid.ui.theme._180LearnAndroidTheme
@@ -18,24 +25,33 @@ class MainActivity2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _180LearnAndroidTheme {
+//            _180LearnAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting2(
                         name = "Android2",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
-            }
+//            }
         }
     }
 }
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var inputText by remember { mutableStateOf("") }
+    Column {
+        Text(
+            text = "Hello $name! say = .......",
+            modifier = modifier
+        )
+        TextField(
+            value = inputText,
+            onValueChange = {
+                inputText = it
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)
